c345e79474a4bddd23d5097e1e1549d28803f712
ivz-dev <zakutynsky@yahoo.com>
2017-06-12 18:36:08 +0300

    test

--- api/getChannelHistory.js
+++ api/getChannelHistory.js
@@ -1,5 +1,6 @@
 const lib       = require('../lib/functions');
 const WebClient = require('@slack/client').WebClient;
+const    datetime = require('node-datetime');
 
 module.exports = (req, res) => {
     let {
@@ -13,6 +14,16 @@ module.exports = (req, res) => {
     } = req.body.args;
 
     if(!token || !channel) throw new Error('Required fields: token, channel');
+
+    if(isNaN(latest)){
+        var dt = datetime.create(latest);
+        latest = dt.epoch();
+    }
+
+    if(isNaN(oldest)){
+        var dt = datetime.create(oldest);
+        oldest = dt.epoch();
+    }
 
     let slack = new WebClient(token);
 
